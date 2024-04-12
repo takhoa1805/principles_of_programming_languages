02f3ca1abadde02b96c0d0e3826ee300ed3917cf
Khoa Tran Anh <khoa.tran1805@hcmut.edu.vn>
2024-04-12 09:44:01 +0700

    make sure everything is up to date

--- assignment_3/assignment3-initial/src/main/zcode/checker/StaticCheck.py
+++ assignment_3/assignment3-initial/src/main/zcode/checker/StaticCheck.py
@@ -7,5 +7,12 @@ from functools import reduce
 
 # MODIFY THIS FILE FOR STATIC CHECKER
 
+
+
 class StaticChecker(BaseVisitor, Utils):
-    pass
+    def __init__(self, ast: Program) -> None:
+        self.ast = ast
+        self.env =[[]]
+
+    def check(self):
+        self.visit(self.ast,self.env)
